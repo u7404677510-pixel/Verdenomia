f718ce2a1a77e4668a70258b1e3eeeb5c43ac3c1
Verdenomia <info@verdenomia.es>
2026-01-05 11:25:35 +0100

    Add WhatsApp button, update phone numbers, fix language toggle, integrate logo

--- next.config.js
+++ next.config.js
@@ -1,7 +1,3 @@
-const createNextIntlPlugin = require('next-intl/plugin')
-
-const withNextIntl = createNextIntlPlugin('./src/i18n/request.ts')
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
@@ -9,4 +5,6 @@ const nextConfig = {
   },
 }
 
-module.exports = withNextIntl(nextConfig)
+module.exports = nextConfig
+
+
